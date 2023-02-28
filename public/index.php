148f6b7de00fa925866e3466cfdbb60e19e04a87
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-02-28 23:57:48 +0100

    twig/template/fichiers

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use \Twig\Loader\FilesystemLoader;
+
+require_once dirname(__DIR__). '/vendor/autoload.php';
+
+$loader = new FilesystemLoader(dirname(__DIR__).'/templates');
+//ici, j'indique à twig que tu vas chercher les fichiers dans le template.
+
+$twig = new \Twig\Environment($loader, [
+    //'cache' => dirname(__DIR__). '/var/cache',
+]);
+
+echo $twig->render('index.html.twig'); //['the' => 'variables', 'go' => 'here']);
+
+//ce fichier se trove dans template.
+//et je l'ai crée dans le template
+
+
+echo "<h1>Bonjour tout le monde monde</h1>";
